00a349cd6ee8199448399ca491723a51dc764be4
debojyoti909 <debojyotid368@gmail.com>
2025-11-05 21:55:40 +0530

    added Threading.java

--- Threading.java
+++ Threading.java
@@ -23,13 +23,29 @@ class Even_Thread extends Thread{
         }
     }
 }
+class Num_Thread extends Thread{
+    public void run(){
+        try{
+            for(int i=0;i<20;i++){
+                System.err.println("number print thread - "+i);;
+                Thread.sleep(100);
+            }
+        }catch (InterruptedException e){
+            System.err.println(e.getMessage());
+        }
+    }
+}
 public class Threading {
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
         int a=sc.nextInt();
         Odd_Thread o=new Odd_Thread();
         Even_Thread ev= new Even_Thread();
+        Num_Thread n= new Num_Thread();
+
         o.start();
         ev.start();
+        n.start();
+
     }
 }
