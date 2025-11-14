b9816619a3315262c9d824e1eea1f02e7e1848bb
debojyoti909 <debojyotid368@gmail.com>
2025-11-14 10:43:25 +0530

    added Increment_Submatrices_by_One_2536.java

---
+++ Increment_Submatrices_by_One_2536.java
@@ -0,0 +1,20 @@
+public class Solution {
+    public int[][] rangeAddQueries(int n, int[][] queries) {
+        int arr[][]=new int[n][n];
+        //Arrays.fill(arr,0);
+        for(int i=0;i<queries.length;i++){
+            int row1=queries[i][0];
+            int col1=queries[i][1];
+            int row2=queries[i][2];
+            int col2=queries[i][3];
+            for(int j=row1;j<=row2;j++){
+                for(int k=col1;k<=col2;k++){
+                    arr[j][k]+=1;
+                }
+            }
+        }
+        return arr;
+    }
+} {
+    
+}
