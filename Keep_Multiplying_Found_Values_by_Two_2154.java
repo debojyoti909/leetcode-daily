91c1b2925957229ce0e24e4bac331502d6462f65
debojyoti909 <debojyotid368@gmail.com>
2025-11-19 10:56:49 +0530

    added Keep_Multiplying_Found_Values_by_Two_2154.java

---
+++ Keep_Multiplying_Found_Values_by_Two_2154.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int findFinalValue(int[] nums, int original) {
+        //int max=Integer.MIN_VALUE;
+        int res=original;
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length;i++){
+           if(nums[i]==original){
+           // res=nums[i];
+            original*=2;
+           }
+           
+        }
+        return original;
+
+    }
+}
