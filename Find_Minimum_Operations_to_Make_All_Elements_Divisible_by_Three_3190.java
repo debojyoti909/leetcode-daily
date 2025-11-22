a638631ec8de230649d68314def5da20f4c982df
debojyoti909 <debojyotid368@gmail.com>
2025-11-22 10:31:12 +0530

    added Find_Minimum_Operations_to_Make_All_Elements_Divisible_by_Three_3190.java

---
+++ Find_Minimum_Operations_to_Make_All_Elements_Divisible_by_Three_3190.java
@@ -0,0 +1,13 @@
+public class Find_Minimum_Operations_to_Make_All_Elements_Divisible_by_Three_3190 {
+    
+    public int minimumOperations(int[] nums) {
+        int cnt=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]%3!=0){
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+}
+
