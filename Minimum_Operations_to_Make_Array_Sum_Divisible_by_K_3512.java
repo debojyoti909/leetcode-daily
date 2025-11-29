f8be487d036b9bfdb291c3fa08adaf3eea72a97d
debojyoti909 <debojyotid368@gmail.com>
2025-11-29 19:38:11 +0530

    added Minimum_Operations_to_Make_Array_Sum_Divisible_by_K_3512.java

---
+++ Minimum_Operations_to_Make_Array_Sum_Divisible_by_K_3512.java
@@ -0,0 +1,11 @@
+public class Minimum_Operations_to_Make_Array_Sum_Divisible_by_K_3512 {
+    
+    public int minOperations(int[] nums, int k) {
+        int sum=0;
+        for(int i=0;i<nums.length;i++){
+            sum+=nums[i];
+        }
+        return sum%k;
+    }
+}
+
