29475fe3ce5db2acce4180c94ca126531ee93622
debojyoti909 <debojyotid368@gmail.com>
2025-12-09 20:30:46 +0530

    added Count_Square_Sum_Triples_1925.java

--- Count_Square_Sum_Triples_1925.java
+++ Count_Square_Sum_Triples_1925.java
@@ -1,6 +1,6 @@
 import java.util.*;
 class Solution {
-    public int countTriples(int n) {
+        public int countTriples(int n) {
         int c=0;
         for(int i=1;i<n-1;i++){
             for(int j=i+1;j<n;j++){
