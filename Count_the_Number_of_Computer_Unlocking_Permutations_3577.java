4256af0b90b9bcfba2d74968890720e0f33d436e
debojyoti909 <debojyotid368@gmail.com>
2025-12-16 21:20:08 +0530

    added Count_the_Number_of_Computer_Unlocking_Permutations_3577.java

--- Count_the_Number_of_Computer_Unlocking_Permutations_3577.java
+++ Count_the_Number_of_Computer_Unlocking_Permutations_3577.java
@@ -8,7 +8,6 @@ public class Count_the_Number_of_Computer_Unlocking_Permutations_3577 {
         }
         int res=1;
         int mod=1000000007;
-        //int mod=1e+7;
         for(int i=2;i<complexity.length;i++){
             res=(int)(((long)res*i)%mod);
         }
