5c3e2cbedbfe5036dd69f4893a4680b8b38e4b3d
debojyoti909 <debojyotid368@gmail.com>
2025-12-24 21:28:00 +0530

    added Apple_Redistribution_Into_Boxes_3074.java

---
+++ Apple_Redistribution_Into_Boxes_3074.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minimumBoxes(int[] apple, int[] capacity) {
+        int sum=0;
+        for(int i=0;i<apple.length;i++){
+            sum+=apple[i];
+        }
+        int sum2=0;
+        Arrays.sort(capacity);
+        int cnt=0;
+        for(int i=capacity.length-1;i>=0;i--){
+            sum2+=capacity[i];
+            cnt++;
+            if(sum<=sum2){
+                return cnt;
+            }
+        }
+        return -1;
+    }
+}
