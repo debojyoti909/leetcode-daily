7e390251b0b9d937ef9064f842439526323c4b59
debojyoti909 <debojyotid368@gmail.com>
2026-01-17 13:19:01 +0530

    added Find_the_Largest_Area_of_Square_Inside_Two_Rectangles_3047.java

---
+++ Find_the_Largest_Area_of_Square_Inside_Two_Rectangles_3047.java
@@ -0,0 +1,21 @@
+class Solution {
+    public long largestSquareArea(int[][] bottomLeft, int[][] topRight) {
+        int n=bottomLeft.length;
+        long ma=0;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                int l=Math.max(bottomLeft[i][0],bottomLeft[j][0]);
+                int r=Math.min(topRight[i][0],topRight[j][0]);
+                int b=Math.max(bottomLeft[i][1],bottomLeft[j][1]);
+                int t=Math.min(topRight[i][1],topRight[j][1]);
+                if(l<r && t>b){
+                    int h=t-b;
+                    int w=r-l;
+                    int side=Math.min(h,w);
+                    ma=Math.max(ma,1L*side*side);
+                }
+            }
+        }
+        return ma;
+    }
+}
