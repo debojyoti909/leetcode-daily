2621fc598a54826ea99e37deb6a08db4b0af8801
debojyoti909 <debojyotid368@gmail.com>
2026-01-31 22:49:21 +0530

    added Find_Smallest_Letter_Greater_Than_Target_744.java

---
+++ Find_Smallest_Letter_Greater_Than_Target_744.java
@@ -0,0 +1,21 @@
+class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+        char res=letters[0];
+        int l=0;
+        int h=letters.length-1;
+        if(letters[h]<=target){
+            return res;
+        }
+        while(l<=h){
+            int mid=(l+h)/2;
+            char ch=letters[mid];
+            if(ch>target){
+                res=ch;
+                h=mid-1;
+            }else{
+                l=mid+1;
+            }
+        }
+        return res;
+    }
+}
