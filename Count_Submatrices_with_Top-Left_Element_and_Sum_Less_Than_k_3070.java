aa23e2e217d73d80a31e4c416de446f62fc6bf3f
debojyoti909 <debojyotid368@gmail.com>
2026-03-18 18:28:35 +0530

    added Count_Submatrices_with_Top-Left_Element_and_Sum_Less_Than_k_3070.java

---
+++ Count_Submatrices_with_Top-Left_Element_and_Sum_Less_Than_k_3070.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int countSubmatrices(int[][] grid, int k) {
+        int col[]=new int[grid[0].length];
+        int res=0;
+        for(int i=0;i<grid.length;i++){
+            int r=0;
+            for(int j=0;j<grid[0].length;j++){
+                col[j]+=grid[i][j];
+                r+=col[j];
+                if(r<=k){
+                    res++;
+                }
+            }
+        }
+        return res;
+    }
+}
