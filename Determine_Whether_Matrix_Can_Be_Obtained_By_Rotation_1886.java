d85f52ae88ff0758978d66750e871c5de57b5561
debojyoti909 <debojyotid368@gmail.com>
2026-04-01 20:11:32 +0530

    added Determine_Whether_Matrix_Can_Be_Obtained_By_Rotation_1886.java

--- Determine_Whether_Matrix_Can_Be_Obtained_By_Rotation_1886.java
+++ Determine_Whether_Matrix_Can_Be_Obtained_By_Rotation_1886.java
@@ -25,7 +25,7 @@ public class Determine_Whether_Matrix_Can_Be_Obtained_By_Rotation_1886 {
                 k--;
             }
         }
-           int c=0;
+        int c=0;
         for(int i=0;i<len;i++){
             for(int j=0;j<len;j++){
                 if(mat[i][j]==target[i][j]){
@@ -36,9 +36,9 @@ public class Determine_Whether_Matrix_Can_Be_Obtained_By_Rotation_1886 {
         if(c==(len*len)){
             return true;
         }else{
-            c=0;
+        c=0;
         }
-            count++;
+        count++;
         }
         return false;
     }
