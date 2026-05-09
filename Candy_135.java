aaff9b13bcf4548ae33fe4481b67b27a9ab72d73
debojyoti909 <debojyotid368@gmail.com>
2026-05-09 22:49:55 +0530

    style: fix indentation in candy method of Candy_135.java

--- Candy_135.java
+++ Candy_135.java
@@ -8,12 +8,12 @@ public class Candy_135 {
         Arrays.fill(can,1);
         for(int i=1;i<n;i++){
             if(ratings[i]>ratings[i-1]){
-                can[i]=can[i-1]+1;
+            can[i]=can[i-1]+1;
             }
         }
         for(int i=n-2;i>=0;i--){
             if(ratings[i]>ratings[i+1]){
-                can[i]=Math.max(can[i],can[i+1]+1);
+            can[i]=Math.max(can[i],can[i+1]+1);
             }
         }
         int res=0;
