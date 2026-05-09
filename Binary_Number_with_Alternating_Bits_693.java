1402fab4acb2293c698cfb34679236c061d43f48
debojyoti909 <debojyotid368@gmail.com>
2026-05-09 22:54:25 +0530

    add Binary_Number_with_Alternating_Bits_693.java with solution to check for alternating bits in a binary number

---
+++ Binary_Number_with_Alternating_Bits_693.java
@@ -0,0 +1,17 @@
+public class Binary_Number_with_Alternating_Bits_693 {
+    class Solution {
+    public boolean hasAlternatingBits(int n) {
+        int chk=-1;
+        while(n!=0){
+            int mod=n%2;
+            if(mod!=chk){
+                chk=mod;
+            }else{
+                return false;
+            }
+            n=n/2;
+        }
+        return true;
+    }
+}
+}
