c0b694a4229278b3d3f9409d9a68adf0608cac79
debojyoti909 <debojyotid368@gmail.com>
2026-05-12 21:55:08 +0530

    add Find_Closest_Person_3516.java with solution to find the closest person based on distance

---
+++ Find_Closest_Person_3516.java
@@ -0,0 +1,15 @@
+public class Find_Closest_Person_3516 {
+    class Solution {
+    public int findClosest(int x, int y, int z) {
+        int a=Math.abs(x-z);
+        int b=Math.abs(y-z);
+        if(a>b){
+            return 2;
+        }else if(b>a){
+            return 1;
+        }else{
+            return 0;
+        }
+    }
+}
+}
