123fc4b003d863e3c444e2f8583e8531a8b11f90
debojyoti909 <debojyotid368@gmail.com>
2026-05-16 11:52:08 +0530

    add Find_Minimum_in_Rotated_Sorted_Array_II_154.java with solution to find the minimum in a rotated sorted array with duplicates

---
+++ Find_Minimum_in_Rotated_Sorted_Array_II_154.java
@@ -0,0 +1,26 @@
+public class Find_Minimum_in_Rotated_Sorted_Array_II_154 {
+    class Solution {
+    public int findMin(int[] nums) {
+      int low=0;
+      int high=nums.length-1;
+      int ans=Integer.MAX_VALUE;
+      while(low<=high){
+        int mid=(low+high)/2;
+        if(nums[low]==nums[mid] && nums[mid]==nums[high]){
+            ans=Math.min(ans,nums[low]);
+            low++;
+            high--;
+            continue;
+        }
+        if(nums[low]<=nums[mid]){
+            ans=Math.min(ans,nums[low]);
+            low=mid+1;
+        }else{
+            ans=Math.min(nums[mid],ans);
+            high=mid-1;
+        }
+      }  
+      return ans;
+    }
+}
+}
